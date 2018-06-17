7eac32c9df001c9bb8a994ebcfa3ee7a32703910
André Krebs <andre.krebssilva@gmail.com>
2018-06-16 22:40:05 -0300

    Add operação de instanciação

--- src/main/java/br/eti/krebscode/ecommercespringboot/EcommercespringbootApplication.java
+++ src/main/java/br/eti/krebscode/ecommercespringboot/EcommercespringbootApplication.java
@@ -1,12 +1,31 @@
 package br.eti.krebscode.ecommercespringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommercespringbootApplication {
+import br.eti.krebscode.ecommercespringboot.domain.Categoria;
+import br.eti.krebscode.ecommercespringboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EcommercespringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercespringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
