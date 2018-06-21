aedcff74b57474509cb95b164b47b98eebe86033
André Krebs <andre.krebssilva@gmail.com>
2018-06-20 23:14:39 -0300

    Paginação com parametros na requisição

--- src/main/java/br/eti/krebscode/ecommercespringboot/services/CategoriaService.java
+++ src/main/java/br/eti/krebscode/ecommercespringboot/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.eti.krebscode.ecommercespringboot.domain.Categoria;
@@ -56,6 +59,12 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPagination(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return categoriaRepository.findAll(pageRequest);
 	}
 	
 }
